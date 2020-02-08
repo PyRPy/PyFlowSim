0d0dbb67cd0d1089f69b4c218069f3288837b88b
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-02-08 15:39:14 -0500

    3D drawings - blender

---
+++ Blender_bpy/create_cone.py
@@ -0,0 +1,23 @@
+import bpy
+
+def delete_all():
+    for item in bpy.context.scene.objects:
+        bpy.context.scene.objects.unlink(item)
+        
+    for item in bpy.data.objects:
+        bpy.data.objects.remove(item)
+        
+    for item in bpy.data.meshes:
+        py.data.meshes.remove(item)
+        
+    for item i bpy.data.materials:
+        bpy.data.materials.remove(item)
+        
+def add_cone():
+    bpy.ops.mesh.primitive_cone_add()
+    
+if __name__ == "__main__":
+    delete_all()
+    add_cone()
+            
+    
